fda028bdc9133b1ef63d6af799822e7081f7b220
garvit.vijai <garvit.vijai@innovaccer.com>
2017-08-21 22:12:45 +0530

    add dummy user endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/setup', function(req, res) {
+  console.log('here');
+  var user = new User({
+    name: 'Garvit',
+    password: 'password',
+    admin: true
+  });
+
+  user.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
 });
 
 module.exports = router;
