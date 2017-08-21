66d999d592031adf38e90346c5fe254de3f2bc96
garvit.vijai <garvit.vijai@innovaccer.com>
2017-08-21 22:13:33 +0530

    adds middleware to verify token

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var authService = require('../services/auth');
+
+router.use(authService.verifyToken);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
 });
 
 module.exports = router;
