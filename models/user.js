74a03724fb2553db2c802509cbaeac913f725cda
garvit.vijai <garvit.vijai@innovaccer.com>
2017-09-04 12:35:45 +0530

    indentation

--- models/user.js
+++ models/user.js
@@ -4,40 +4,41 @@ var bcrypt          = require('bcrypt');
 var SALT_WORK_FACTOR    = 10;
 
 var userSchema = new Schema({
-    name: String,
-    password: String,
-    admin: Boolean,
-    googleId: String,
-    provider: String
+  name: String,
+  username: { type: String, unique: true, required: true },
+  password: String,
+  admin: Boolean,
+  googleId: { type: String, unique: true },
+  provider: String
 });
 
 userSchema.pre('save', function(next) {
-    var user = this;
+  var user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if ( ! user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if ( ! user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
 
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
     });
+  });
 });
 
 userSchema.methods.validPassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
 
-        cb(null, isMatch);
-    });
+    cb(null, isMatch);
+  });
 };
 
 module.exports = mongoose.model('User', userSchema);
